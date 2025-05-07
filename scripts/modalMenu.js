91e54e25b2c92c23ff3fec0041e09b35fefbcd9f
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-05-07 11:57:28 +0300

    ++

--- scripts/modalMenu.js
+++ scripts/modalMenu.js
@@ -9,6 +9,17 @@ document.querySelector(".burger-top").addEventListener("click", () => {
   }
 });
 
+document.querySelector(".burger-top1").addEventListener("click", () => {
+  if (
+    (document.querySelector(".menu-footer").style.display =
+      "" || document.querySelector(".menu-footer").style.display === "block")
+  ) {
+    document.querySelector(".menu-footer").style.display = "none";
+  } else {
+    document.querySelector(".menu-footer").style.display = "block";
+  }
+});
+
 document.querySelector("#btn_close_menu").addEventListener("click", () => {
   document.querySelector(".menu-footer").style.display = "none";
 });
